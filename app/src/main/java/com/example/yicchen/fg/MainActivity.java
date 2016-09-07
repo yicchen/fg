1c02c34026def0b2620579ead80ec30eb77543a7
yicchen <yicchen.cs03@g2.nctu.edu.tw>
2016-09-07 23:34:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yicchen/fg/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.yicchen.fg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
